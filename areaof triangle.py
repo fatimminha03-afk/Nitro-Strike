e07e84cd1add7c2552c8f28cbc3e83e3eb961edc
fatimminha03-afk <fatimminha03@gmail.com>
2025-11-07 18:56:49 +0530

    Add files via upload

---
+++ areaof triangle.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 11:26:20 2022
+
+@author: Dell
+"""
+
+
+B=10
+H=5
+area=1/2*H*B
+print(area)
+a=12
+b=10
+c=5
+s=(a+b+c)/2
+a=[s*(s-a)*(s-b)*(s-c)**1/2]
+print("Area of triangle using herons formula",a)
